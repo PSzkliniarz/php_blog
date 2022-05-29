3a848a4e711619e5e9ad36c54ce2711f4ea6896c
PSzkliniarz <pszkliniarz@gmail.com>
2022-05-29 22:21:15 +0200

    Added post, category, comment models

---
+++ blog_v1/app/src/Entity/Comment.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $comment_text;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $autor;
+
+    #[ORM\ManyToOne(targetEntity: Post::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private $post_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommentText(): ?string
+    {
+        return $this->comment_text;
+    }
+
+    public function setCommentText(string $comment_text): self
+    {
+        $this->comment_text = $comment_text;
+
+        return $this;
+    }
+
+    public function getAutor(): ?string
+    {
+        return $this->autor;
+    }
+
+    public function setAutor(string $autor): self
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    public function getPostId(): ?Post
+    {
+        return $this->post_id;
+    }
+
+    public function setPostId(?Post $post_id): self
+    {
+        $this->post_id = $post_id;
+
+        return $this;
+    }
+}
