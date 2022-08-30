649fcefd7b1f44b1d058326f0c7fa32faf98ef1f
PSzkliniarz <pszkliniarz@gmail.com>
2022-08-30 09:43:20 +0200

    Added service to category

--- blog_v1/app/src/Form/CategoryType.php
+++ blog_v1/app/src/Form/CategoryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,9 +12,14 @@ class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('name')
-        ;
+        $builder->add(
+            'name',
+            TextType::class,
+            [
+                'label' => 'label.name',
+                'required' => true,
+                'attr' => ['max_length' => 255],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -21,5 +27,17 @@ class CategoryType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Category::class,
         ]);
+    }
+
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     *
+     * The block prefix defaults to the underscored short class name with
+     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'category';
     }
 }
